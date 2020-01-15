c0a918a59a667e1d94a4284334f8d1ff1a1369d0
Charles Syperski <csyperski@gmail.com>
2020-01-15 15:09:16 +0000

    Fixed issue with schema update

--- src/main/java/com/cwssoft/reportout/db/DefaultDatabaseManager.java
+++ src/main/java/com/cwssoft/reportout/db/DefaultDatabaseManager.java
@@ -1,9 +1,5 @@
 package com.cwssoft.reportout.db;
 
-import java.util.ArrayList;
-import java.util.List;
-import javax.inject.Inject;
-
 import com.cwssoft.reportout.dao.SettingsDao;
 import com.cwssoft.reportout.service.SettingsService;
 import lombok.Getter;
@@ -13,8 +9,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.inject.Inject;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
- *
  * @author csyperski
  */
 @Slf4j
@@ -48,7 +48,7 @@ public class DefaultDatabaseManager implements DatabaseManager {
     public int getTargetVersion() {
         return getPackages()
                 .stream()
-                .mapToInt(up -> up.getToVersion() )
+                .mapToInt(up -> up.getToVersion())
                 .max()
                 .orElse(NOT_INSTALLED);
     }
@@ -70,17 +70,21 @@ public class DefaultDatabaseManager implements DatabaseManager {
     @Override
     public boolean updateInternal(int from, int to) throws DbUpdateException {
         log.info("Upgrading from version " + from + " to " + to + ".");
-        for (UpdatePackage p : (packages != null ? packages : new ArrayList<UpdatePackage>() ) ) {
-            if (p == null) {
-                continue;
-            }
+        if (packages != null) {
+            final List<UpdatePackage> sortedPackages = packages.stream()
+                    .filter(p -> p != null)
+                    .sorted(Comparator.comparing(UpdatePackage::getFromVersion))
+                    .collect(Collectors.toList());
 
-            if (from == p.getFromVersion()) {
-                if (!p.runUpgrade()) {
-                    throw new DbUpdateException("Failed to update schema from " + from + " to " + p.getToVersion());
-                } else {
-                    from = getCurrentSchemaVersion();
-                    log.info("Current database version = " + from);
+            int current = from;
+            for (UpdatePackage p : sortedPackages) {
+                if (current == p.getFromVersion()) {
+                    if (!p.runUpgrade()) {
+                        throw new DbUpdateException("Failed to update schema from " + current + " to " + p.getToVersion());
+                    } else {
+                        current = getCurrentSchemaVersion();
+                        log.info(" ** Current database version = " + current);
+                    }
                 }
             }
         }
